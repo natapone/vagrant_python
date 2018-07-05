0c066847d7a5dc749fef033c7c21fae20797e26f
Natapone C <natapone@gmail.com>
2018-07-05 10:40:45 +0630

    Pre install python3

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # vagrant_python
 Vagrant box for Python on Ubuntu
+
+# Project folder
+$ cd /home/vagrant/project/Python-Data-Science-and-Machine-Learning-Bootcamp
+
+# Run jupyter
+$ jupyter notebook --ip=33.33.33.2
+
+# Install Python package
+$ pip3 install --user <package name>
--- vagrant/script/bootstrap.sh
+++ vagrant/script/bootstrap.sh
@@ -1,11 +1,26 @@
 apt-get update --force-yes
-apt-get -f install
+apt-get upgrade -y
 apt-get install -y curl
 
-# # Install Etherium
-# apt-get -f install software-properties-common
-# add-apt-repository -y ppa:ethereum/ethereum
-# apt-get update --force-yes
-# apt-get install -y ethereum
-# apt-get install -y ntp # Automatic time from the Internet
-# service ntp start
+# Fix local
+echo -e 'LANGUAGE=en_US.UTF-8
+LANG=en_US.UTF-8
+LC_ALL=en_US.UTF-8
+LC_CTYPE=en_US.UTF-8' > /etc/default/locale
+
+# install python
+echo '+++++++++ Install Python +++++++++'
+apt-get install build-essential -q -y
+apt-get install python3 python3-pip python3-dev -y
+
+# Upgrade PIP
+python3 -m pip install --upgrade pip
+pip install --upgrade pip
+
+# Install Jupyter
+python3 -m pip install jupyter
+
+
+# Install TensorFlow
+pip3 install --user tensorflow
+pip3 install --user tensorflow-gpu
