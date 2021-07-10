e4f6c2f49de0a2d7a2ffe3360c2de37ec9805824
Natapone Charsombut <natapone@gmail.com>
2021-07-10 12:43:41 +0700

    Update bootstrap.sh

--- vagrant/script/bootstrap.sh
+++ vagrant/script/bootstrap.sh
@@ -1,26 +1,15 @@
-apt-get update --force-yes
+apt-get update -y
 apt-get upgrade -y
 apt-get install -y curl
 
-# Fix local
-echo -e 'LANGUAGE=en_US.UTF-8
-LANG=en_US.UTF-8
-LC_ALL=en_US.UTF-8
-LC_CTYPE=en_US.UTF-8' > /etc/default/locale
+
 
 # install python
 echo '+++++++++ Install Python +++++++++'
-apt-get install build-essential -q -y
-apt-get install python3 python3-pip python3-dev -y
+apt-get install make build-essential -y
+
+apt-get install python3-pip python3-dev -y
+apt-get install python3-venv
 
 # Upgrade PIP
-python3 -m pip install --upgrade pip
-pip install --upgrade pip
-
-# Install Jupyter
-python3 -m pip install jupyter
-
-
-# Install TensorFlow
-pip3 install --user tensorflow
-pip3 install --user tensorflow-gpu
+python3 -m pip install pip
